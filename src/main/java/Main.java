a0adc101c344e0a646a343ffb76b59cedba44fe0
WHSLernBot <whslernbot@gmail.com>
2016-11-23 14:15:12 +0100

    Spark Kommunikation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,24 +9,8 @@ public class Main {
 
   public static void main(String[] args) {
       
- 
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-  
-    get("/test", (req, res) -> {
-        
-        return "Sampletext Test";
-        
-    }, responseTransformer.JsonUtil.json());
+      get("/hello", (req, res) -> "Hello World");
     
-    
-
-    get("/", (req, res) -> {
-            
-            res.status(400);
-            return "No access: Contact whslernbot@gmail.com";
-        });
     
   }
     
