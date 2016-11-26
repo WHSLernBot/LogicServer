84793454926c2ca28337b3b63d21c73c2e00f532
WHSLernBot <whslernbot@gmail.com>
2016-11-26 20:39:11 +0100

    Heroku

--- src/main/java/spark/Main.java
+++ src/main/java/spark/Main.java
@@ -1,3 +1,5 @@
+package spark;
+
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -12,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
       
-        get("/hello", (req, res) -> {
+        get("/db", (req, res) -> {
             
             
             JsonParser jp = new JsonParser();
