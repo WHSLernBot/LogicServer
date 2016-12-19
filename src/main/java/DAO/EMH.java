044a7959f821c037c95144e1e5d320ff3fe24244
WHSLernBot <whslernbot@gmail.com>
2016-12-19 14:18:46 +0100

    Läuft auf Heroku Ohne Datenbank anbindung

--- src/main/java/DAO/EMH.java
+++ src/main/java/DAO/EMH.java
@@ -1,8 +1,12 @@
 package DAO;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import static spark.Spark.port;
 
 public class EMH {
 
@@ -10,7 +14,24 @@ public class EMH {
     private static final ThreadLocal<EntityManager> THREAD_LOCAL;
 
     static {
-        EMF = Persistence.createEntityManagerFactory("LernServletPU");
+        
+                String databaseUrl = System.getenv("DATABASE_URL");
+                StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+                String dbVendor = st.nextToken(); //if DATABASE_URL is set
+                String userName = st.nextToken();
+                String password = st.nextToken();
+                String host = st.nextToken();
+                String port = st.nextToken();
+                String databaseName = st.nextToken();
+                String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
+                Map<String, String> properties = new HashMap<>();
+                properties.put("javax.persistence.jdbc.url", jdbcUrl );
+                properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+                properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+                EMF = Persistence.createEntityManagerFactory("LernServletPU", properties);
+        
+        
+//        EMF = Persistence.createEntityManagerFactory("LernServletPU");
         THREAD_LOCAL = new ThreadLocal<>();
     }
 
--- src/main/java/spark/Main.java
+++ src/main/java/spark/Main.java
@@ -1,45 +1,123 @@
 package spark;
 
-
-import com.google.gson.JsonElement;
+import Message.Nachricht;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import main.ChatBotManager;
-import static spark.Spark.*;
-
+import static spark.Service.ignite;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 public class Main {
     
-    
+    private static final int POOL_SIZE = 20;
+   
 
     public static void main(String[] args) {
         
+        igniteFirstSpark();  
+       
+    }
+    
+//    static void igniteSecondSpark() {     
+//        
+//        Service http = ignite();
+//        
+//        
+//        http.get("/basic", (req, res) -> "Hello World 2");
+//        
+//        
+//    }
+    
+    static void igniteFirstSpark() {
         
-        get("/", (req, res) -> {
+        Service http = ignite()
+                      .port(getHerokuAssignedPort())
+                      .threadPool(POOL_SIZE);
+        
+        
+        http.get("/", (req, res) -> "Ja es geht zumindest.");
+        
+        http.post("/messageBot", (req, res) -> {
             
-           
-            return "Das ist meine Hauptseite";
+            JsonParser parser = new JsonParser();
+            
+            String s = req.body();
+            
+            JsonObject obj= new JsonObject();
+            
+            JsonObject objBody;
+            
+            obj.add("body", parser.parse(s));
+            objBody = obj.get("body").getAsJsonObject();
             
             
-        });
+            Rand rd = new Rand(objBody.get("key").getAsInt());
+                
+            
+            System.out.println(rd.toString());
+            return rd.toString() + ". Zeit: " + ChatBotManager.getInstance().jetzt();
       
-        get("/db", (req, res) -> {
-            
-            
-//            JsonParser jp = new JsonParser();
-//            
-//            JsonElement je = jp.parse(req.body());
-//            
-//            JsonObject jo = je.getAsJsonObject();
-//            
-//            res.status(200);
-//            res.type("application/json");
-
-            return "Laeuft etwas";
         });
+        
+        
+        http.get("/hello2", (q, a) -> "Hello from port 1234!");
+        
+        
+        http.get("/hello3", (q, a) -> "Hello from port 5678!");
+        
+        
+        
+    }
     
-    
-  }
-    
+    public void sendMessage(String route, Nachricht nachricht) throws UnsupportedEncodingException {
+        
+        Nachricht message = nachricht;
+        
+        JsonObject jo = message.getJson();
+        Gson gson = new Gson();
+        String postUrl = route;
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(postUrl);
+        StringEntity postingString = new StringEntity(gson.toJson(jo));
+        post.setEntity(postingString);
+        post.setHeader("Content-type", "application/json");
+        try {
+            
+            HttpResponse response = httpClient.execute(post);
+            
+            StatusLine sl = response.getStatusLine();
+            
+            if(sl.getStatusCode() != 200) {
+                
+                //I.wie Sammmeln wenn was schiefgegangen ist und erneut versuchen.
+                HashMap<String, String> m = new HashMap<>();
+                 
+            }
+            
+        } catch (IOException ex) {
+            
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //Port welcher Standardmäßig verwendet werden soll.
+    }
 
 }
